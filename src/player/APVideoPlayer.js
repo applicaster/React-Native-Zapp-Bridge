8013a574188b9365f2dcddf4e6737fdfa5275508
saracodes <sara@sara.codes>
2017-11-21 02:11:01 +0200

    feat: APVideoPlayer

---
+++ src/player/APVideoPlayer.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  requireNativeComponent,
+  View,
+  StyleSheet,
+} from 'react-native';
+import { getFrameDims } from '../utils/';
+
+const styles = StyleSheet.create({
+  video: {
+    backgroundColor: '#000000',
+  },
+});
+
+const APReactVideoView = {
+  name: 'ReactVideoView',
+  propTypes: {
+    src: PropTypes.shape({
+      type: PropTypes.string,
+      object: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.shape({
+          playerDetailsObject: PropTypes.shape({
+            id: PropTypes.string,
+            name: PropTypes.string,
+            thumbnail_url: PropTypes.string,
+            stream_url: PropTypes.string,
+          }),
+        }),
+      ]),
+      player_configuration: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.shape({
+          inline_player_should_auto_mute: PropTypes.boolean,
+        }),
+      ]),
+      startTime: PropTypes.string,
+    }),
+    maxWidth: PropTypes.string,
+    ...View.propTypes,
+  },
+};
+
+const ReactVideoView = requireNativeComponent('APReactVideoView', APReactVideoView);
+
+const APVideoPlayer = ({ src, maxWidth, ratio }) => {
+  const dims = getFrameDims(maxWidth, ratio);
+  return (
+    <ReactVideoView src={src} style={[dims, styles.video]} />
+  );
+};
+
+APVideoPlayer.propTypes = {
+  src: PropTypes.object.isRequired,
+  maxWidth: PropTypes.number,
+  ratio: PropTypes.number,
+};
+
+APVideoPlayer.defaultProps = {
+  maxWidth: null,
+  ratio: 9 / 16,
+};
+
+export default APVideoPlayer;
---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+import { Dimensions } from 'react-native';
+
+export const getFrameDims = (maxWidth, ratio = 9 / 16) => {
+  const width = maxWidth || Dimensions.get('window').width;
+  const getFrameHeight = w => Math.floor(w * ratio);
+  return ({
+    width,
+    height: getFrameHeight(width),
+  });
+};
