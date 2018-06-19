d410187bdacda99c03508afa0dbdeec28e225de3
Ritchie Comley <ritchie.comley@gmail.com>
2018-06-19 19:03:07 +0100

    feat: makes styles consistent between platforms

---
+++ test/withZapp/withZapp.test.js
@@ -0,0 +1,88 @@
+import { propsParser } from '../../src/withZapp';
+
+describe('withZapp HOC', () => {
+  describe('propsParser(props, platform)', () => {
+    it('will correctly parse values for android', () => {
+      expect(propsParser({ foo: 'bar' }, 'android')).toMatchObject({
+        foo: 'bar'
+      });
+      expect(
+        propsParser({ extra_props: '{"foo": "bar"}' }, 'android')
+      ).toMatchObject({
+        extra_props: { foo: 'bar' }
+      });
+      expect(
+        propsParser({ extra_props: '{"foo": "bar"}' }, 'ios')
+      ).toMatchObject({
+        extra_props: '{"foo": "bar"}'
+      });
+    });
+
+    it('will cope with realish android props', () => {
+      expect(
+        propsParser(
+          {
+            is_tablet: false,
+            localization: JSON.stringify({}),
+            settings: JSON.stringify({}),
+            styles: JSON.stringify({
+              tablet: { plat: 'tablet' },
+              smartphone: { plat: 'smartphone' }
+            }),
+            extra_props: JSON.stringify({})
+          },
+          'android'
+        )
+      ).toMatchObject({
+        is_tablet: false,
+        localization: {},
+        settings: {},
+        styles: { plat: 'smartphone' },
+        extra_props: {}
+      });
+
+      expect(
+        propsParser(
+          {
+            is_tablet: true,
+            localization: JSON.stringify({}),
+            settings: JSON.stringify({}),
+            styles: JSON.stringify({
+              tablet: { plat: 'tablet' },
+              smartphone: { plat: 'smartphone' }
+            }),
+            extra_props: JSON.stringify({})
+          },
+          'android'
+        )
+      ).toMatchObject({
+        is_tablet: true,
+        localization: {},
+        settings: {},
+        styles: { plat: 'tablet' },
+        extra_props: {}
+      });
+    });
+
+    it('will cope with realish ios props', () => {
+      expect(
+        propsParser(
+          {
+            is_tablet: false,
+            localization: {},
+            settings: {},
+            styles: { plat: 'orig' },
+            extra_props: {}
+          },
+          'ios'
+        )
+      ).toMatchObject({
+        is_tablet: false,
+        localization: {},
+        settings: {},
+        styles: { plat: 'orig' },
+        extra_props: {}
+      });
+    });
+  });
+});
