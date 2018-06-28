c5f00c83b46be394db44f8be65955d0d3d809554
Ritchie Comley <ritchie.comley@gmail.com>
2018-06-28 09:59:07 +0100

    chore: remove export for zapp-plugin

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ export { ZappPipesService } from './src/datasource/ZappPipesService';
 export { default as reminders } from './src/reminders';
 export { default as navigation } from './src/navigation';
 export { default as withZapp } from './src/withZapp';
-export { default as zappPlugin } from './src/zapp-plugin';
